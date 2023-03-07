4cf1a42cd0796a57892a6751ae0ae84fd82a46e4
Amogh2009 <amogh.gupta17@gmail.com>
2023-03-07 07:12:39 -0800

    set up inertial turns
    - clockwise
    and
    - counter clockwise

--- src/inertial.cpp
+++ src/inertial.cpp
@@ -2,7 +2,7 @@
 
 using namespace vex;
 
-void inertialTurnHeading(double targetHeading) {
+void inertialTurnClockwise(double targetHeading) {
   double currentError = targetHeading - Inertial.heading();
   double Kp = 0.5;
   double motorSpeed;
@@ -16,6 +16,31 @@ void inertialTurnHeading(double targetHeading) {
     RightFront.spin(reverse, motorSpeed, pct);
     RightMiddle.spin(reverse, motorSpeed, pct);
     RightBack.spin(reverse, motorSpeed, pct);
+
+    currentError = targetHeading - Inertial.heading();
+  }
+  LeftFront.stop(hold);
+  LeftMiddle.stop(hold);
+  LeftBack.stop(hold);
+  RightFront.stop(hold);
+  RightMiddle.stop(hold);
+  RightBack.stop(hold); 
+} 
+
+void inertialTurnCounterClockwise(double targetHeading) {
+  double currentError = targetHeading - Inertial.heading();
+  double Kp = 0.5;
+  double motorSpeed;
+
+  while(fabs(currentError) < 4) {
+    motorSpeed = Kp * currentError;
+
+    LeftFront.spin(reverse, motorSpeed, pct);
+    LeftMiddle.spin(reverse, motorSpeed, pct);
+    LeftBack.spin(reverse, motorSpeed, pct);
+    RightFront.spin(fwd, motorSpeed, pct);
+    RightMiddle.spin(fwd, motorSpeed, pct);
+    RightBack.spin(fwd, motorSpeed, pct);
 
     currentError = targetHeading - Inertial.heading();
   }
